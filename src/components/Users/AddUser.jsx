9734e92d889eab44ac0f4d72e71191c96f130752
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2023-02-20 11:44:59 +0530

    Refactor to manage user input state

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,19 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import Card from '../UI/Card'
 import classes from './AddUser.module.css';
 import Button from "../UI/Button";
 
 const AddUser = (props)=>{
+    const [enteredUserName,setEnteredUserName]= useState('');
+    const [enteredAge,setEnteredAge]= useState('');
     const addUserHandler=(event)=>{
         event.preventDefault();
+    };
+
+    const userNameChangeHandler =(event)=>{
+        setEnteredUserName(event.target.value);
+    };   
+    const ageChangeHandler =(event)=>{
+        setEnteredAge(event.target.value);
     };
     return(
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text"></input>
+            <input id="username" type="text" onChange={userNameChangeHandler}></input>
             <label htmlFor="age">Age</label>
-            <input id="age" type="number"></input>
+            <input id="age" type="number" onChange={ageChangeHandler}></input>
             <Button type="submit">Add User</Button>
         </form>
         </Card>
